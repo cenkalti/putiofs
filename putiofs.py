70a661474478871be374013a362bad1321b56ecd
Cenk Alti <cenkalti@gmail.com>
2016-01-04 23:39:09 +0200

    reset

--- putiofs.py
+++ putiofs.py
@@ -1,185 +1,179 @@
 #!/usr/bin/env python
-
-import os
-import logging
 import argparse
+import logging
+from errno import ENOENT, EROFS, ENOTSUP, EIO
+from stat import S_IFDIR
 
-from errno import ENOENT, EROFS
-from stat import S_IFDIR, S_IFLNK, S_IFREG
-from sys import argv, exit
-from time import time
-from tempfile import NamedTemporaryFile
-from pdb import set_trace as st
+import putio
+from fuse import FUSE, LoggingMixIn, Operations, FuseOSError
 
-import putio2
-from fuse import FUSE, FuseOSError, Operations, LoggingMixIn
-
-now = time()
 
 class PutioFS(LoggingMixIn, Operations):
-    """Implementation of put.io filesystem"""
-    
+
     def __init__(self):
-        self.fd = 0
-        self.temporary_files = {} # buffer area before uploading {path: TemporaryFile}
-        self._fetch_files()
-    
-    def _fetch_files(self):
-        '''Fetch all files from put.io''' 
-        
-        # -1 means all files
-        self.files = client.File.list(-1, as_dict=True)
-        # as {id: file}
-        
-        # create root
-        self.files[0] = client.File(dict(id=0, name='Your Files',
-            content_type='application/x-directory',
-            parent_id=None))
-        
-        # attact stat object to files
-        for f in self.files.values():
-            self._attach_stat(f)
-        
-        # same files by indexed with path
-        self.path_files = {}
-        for f in self.files.values():
-            path = self._construct_path(f)
-            self.path_files[path] = f
-    
-    def _add_to_files(self, file):
-        self.files[file.id] = file
-        path = self._construct_path(file)
-        self.path_files[path] = file
-        self._attach_stat(file)
-    
-    def _attach_stat(self, file):
-        if file.content_type == 'application/x-directory':
-            mode = (S_IFDIR | 0700)
-            size = 0
-        else:
-            mode = (S_IFREG | 0400)
-            size = file.size
-            
-        file.stat = dict(
-            st_mode=mode,
-            st_ctime=now,
-            st_atime=now,
-            st_size=size,
-            st_blksize=1000000
-            # st_blksize a filesystem-specific preferred I/O block size for this object.
-            # but, nobody seems to respect it.
-        )
-            
-    def _construct_path(self, file):
-        '''For given file, returns the path to the mount point'''
-        
-        if file.id == 0:
-            return '/'
-        
-        path = str(file)
+        pass
 
-        parent_id = file.parent_id
-        while parent_id:
-            parent = self.files[parent_id]
-            path = str(parent) + '/' + path
+    def access(self, path, amode):
+        return 0
 
-            parent_id = parent.parent_id
+    bmap = None
 
-        return '/' + path
-        
-    def _get_file_by_path(self, path):
-        return self.path_files[path]
-    
-    def _get_file_by_id(self, id):
-        return self.files[id]
-    
-    def _children(self, file):
-        return filter(lambda f: f.parent_id == file.id, self.files.values())
-         
-    def create(self, path, mode):
-        if path in self.temporary_files:
-            raise FuseOSError(EROFS)
-        
-        self.temporary_files[path] = NamedTemporaryFile(delete=False)
+    def chmod(self, path, mode):
+        raise FuseOSError(EROFS)
 
-        self.fd += 1
-        return self.fd
-    
+    def chown(self, path, uid, gid):
+        raise FuseOSError(EROFS)
+
+    def create(self, path, mode, fi=None):
+        """
+        When raw_fi is False (default case), fi is None and create should
+        return a numerical file handle.
+
+        When raw_fi is True the file handle should be set directly by create
+        and return 0.
+        """
+
+        raise FuseOSError(EROFS)
+
+    def destroy(self, path):
+        """Called on filesystem destruction. Path is always /"""
+
+        pass
+
+    def flush(self, path, fh):
+        return 0
+
+    def fsync(self, path, datasync, fh):
+        return 0
+
+    def fsyncdir(self, path, datasync, fh):
+        return 0
+
     def getattr(self, path, fh=None):
-        try:
-            f = self.temporary_files[path]
-        except KeyError:
-            pass
-        else:
-            st = os.lstat(f.name)
-            return dict((key, getattr(st, key)) for key in ('st_atime', 'st_ctime',
-                'st_gid', 'st_mode', 'st_mtime', 'st_nlink', 'st_size', 'st_uid'))
-            
-        if path not in self.path_files:
+        """
+        Returns a dictionary with keys identical to the stat C structure of
+        stat(2).
+
+        st_atime, st_mtime and st_ctime should be floats.
+
+        NOTE: There is an incombatibility between Linux and Mac OS X
+        concerning st_nlink of directories. Mac OS X counts all files inside
+        the directory, while Linux counts only the subdirectories.
+        """
+
+        if path != '/':
             raise FuseOSError(ENOENT)
-            
-        file = self._get_file_by_path(path)
-        return file.stat
-    
+        return dict(st_mode=(S_IFDIR | 0755), st_nlink=2)
+
+    def getxattr(self, path, name, position=0):
+        raise FuseOSError(ENOTSUP)
+
+    def init(self, path):
+        """
+        Called on filesystem initialization. (Path is always /)
+
+        Use it instead of __init__ if you start threads on initialization.
+        """
+
+        pass
+
+    def link(self, target, source):
+        """creates a hard link `target -> source` (e.g. ln source target)"""
+
+        raise FuseOSError(EROFS)
+
+    def listxattr(self, path):
+        return []
+
+    lock = None
+
     def mkdir(self, path, mode):
-        dirname, basename = os.path.split(path)
-        parent = self._get_file_by_path(dirname)
-        newdir = client.File.create_folder(parent_id=parent.id)
-        self._add_to_files(newdir)
+        raise FuseOSError(EROFS)
+
+    def mknod(self, path, mode, dev):
+        raise FuseOSError(EROFS)
 
     def open(self, path, flags):
-        self.fd += 1
-        return self.fd
-    
+        """
+        When raw_fi is False (default case), open should return a numerical
+        file handle.
+
+        When raw_fi is True the signature of open becomes:
+            open(self, path, fi)
+
+        and the file handle should be set directly.
+        """
+
+        return 0
+
+    def opendir(self, path):
+        """Returns a numerical file handle."""
+
+        return 0
+
     def read(self, path, size, offset, fh):
-        f = self._get_file_by_path(path)
-        return f.download(range=(offset, offset+size))
-    
+        """Returns a string containing the data requested."""
+
+        raise FuseOSError(EIO)
+
     def readdir(self, path, fh):
-        f = self._get_file_by_path(path)
-        children = self._children(f)
-        return ['.', '..'] + [str(c) for c in children]
-    
+        """
+        Can return either a list of names, or a list of (name, attrs, offset)
+        tuples. attrs is a dict as in getattr.
+        """
+
+        return ['.', '..']
+
+    def readlink(self, path):
+        raise FuseOSError(ENOENT)
+
     def release(self, path, fh):
-        try:
-            f = self.temporary_files[path]
-        except KeyError:
-            # no need to close files which are opened for reading
-            pass
-        else:
-            temppath = f.name
-            f.close()
-        
-            try:
-                filename = os.path.basename(path)
-                newfile = client.File.upload(temppath, filename)
-                self._add_to_files(newfile)
-            finally:
-                os.remove(temppath)
-                del self.temporary_files[path]
+        return 0
+
+    def releasedir(self, path, fh):
+        return 0
+
+    def removexattr(self, path, name):
+        raise FuseOSError(ENOTSUP)
 
     def rename(self, old, new):
-        f = self._get_file_by_path(old)
-        f.rename(new)
-        del self.path_files[path]
-        newpath = self._construct_path(f)
-        self.path_files[path] = f
-         
+        raise FuseOSError(EROFS)
+
     def rmdir(self, path):
-        f = self._get_file_by_path(path)
-        id = f.id
-        f.delete()
-        del self.files[id]
-        del self.path_files[path]
-    
+        raise FuseOSError(EROFS)
+
+    def setxattr(self, path, name, value, options, position=0):
+        raise FuseOSError(ENOTSUP)
+
     def statfs(self, path):
-        return dict(f_bsize=512, f_blocks=4096, f_bavail=2048)
-    
+        """
+        Returns a dictionary with keys identical to the statvfs C structure of
+        statvfs(3).
+
+        On Mac OS X f_bsize and f_frsize must be a power of 2
+        (minimum 512).
+        """
+
+        return {}
+
+    def symlink(self, target, source):
+        """creates a symlink `target -> source` (e.g. ln -s source target)"""
+
+        raise FuseOSError(EROFS)
+
+    def truncate(self, path, length, fh=None):
+        raise FuseOSError(EROFS)
+
+    def unlink(self, path):
+        raise FuseOSError(EROFS)
+
+    def utimens(self, path, times=None):
+        """Times is a (atime, mtime) tuple. If None use current time."""
+
+        return 0
+
     def write(self, path, data, offset, fh):
-        f = self.temporary_files[path]
-        f.seek(offset)
-        f.write(data)
-        return len(data)
+        raise FuseOSError(EROFS)
 
 
 if __name__ == "__main__":
@@ -188,9 +182,9 @@ if __name__ == "__main__":
     parser.add_argument('oauth_token')
     args = parser.parse_args()
     
-    logger = logging.getLogger('putio2')
+    logger = logging.getLogger('putio')
     logging.basicConfig(level=logging.DEBUG)
     
-    client = putio2.Client(args.oauth_token)
+    client = putio.Client(args.oauth_token)
     
     fuse = FUSE(PutioFS(), args.mount_point, foreground=True)
